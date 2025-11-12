59f8be849d66d31ed56f3da8d9ca73ca1e37ca5f
Michael Stephen Daniel Panjaitan <michael.stephen@ui.ac.id>
2025-11-12 21:51:39 +0700

    Tutorial 8 completed

--- lib/widgets/left_drawer.dart
+++ lib/widgets/left_drawer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:football_news/screens/menu.dart';
 import 'package:football_news/screens/newslist_form.dart';
+import 'package:football_news/screens/news_entry_list.dart';
 
 class LeftDrawer extends StatelessWidget {
   const LeftDrawer({super.key});
@@ -66,6 +67,16 @@ class LeftDrawer extends StatelessWidget {
 			  title: const Text("See Football News"),
 			  onTap: () {
 
+			  },
+		  ),
+
+		  ListTile(
+			  leading: const Icon(Icons.add_reaction_rounded),
+			  title: const Text("News List"),
+			  onTap: () {
+				Navigator.push(context,
+				MaterialPageRoute(builder: (context) => const NewsEntryListPage()),
+				);
 			  },
 		  ),
         ],
